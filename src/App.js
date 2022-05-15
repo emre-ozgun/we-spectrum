2e5c8b2deced93531308d15c25874971fc6f91db
emre-ozgun <emre2996ozgun@gmail.com>
2022-05-15 19:42:01 +0300

    add earthquake data (json)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { data } from './data';
 
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<h1>{data.length}</h1>
+		</div>
+	);
 }
 
 export default App;
