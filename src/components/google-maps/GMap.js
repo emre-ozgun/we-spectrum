b45bf36402a3afa136d8f8b1637ce9a24681471f
Emre Oz <emreozgun@Emres-MacBook-Pro.local>
2022-05-19 22:41:43 +0300

    draw line between user provided coords and closest coords based on earthquake data

--- src/components/google-maps/GMap.js
+++ src/components/google-maps/GMap.js
@@ -4,15 +4,19 @@ import {
 	withScriptjs,
 	withGoogleMap,
 	Marker,
+	Polyline
 } from 'react-google-maps';
 import { MAPS_URL } from '../../mapsKey';
 import { selectMapData, setCoords } from '../../features/mapData/mapDataSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import { mapStyles } from '../../utils/mapStyles';
+import PolylineCoordinates from '../../polyline.json';
 
 const GMap = () => {
 	const dispatch = useDispatch();
-	const { lat, long, closestCoords } = useSelector(selectMapData);
+	const { lat, long, closestCoords, formDataComposite } = useSelector(selectMapData);
+
+
 
 	const handleCoordChange = (e) => {
 		const lat = e.latLng.lat();
@@ -20,9 +24,21 @@ const GMap = () => {
 		dispatch(setCoords({ lat, lng }));
 	};
 
+	const lineBetween = [
+		{
+			lat: Number(closestCoords.latClosest),
+			lng: Number(closestCoords.lngClosest)
+		},
+		{
+			lat: Number(formDataComposite.formLat),
+			lng: Number(formDataComposite.formLong)
+		}
+	];
 
+	const canDraw = (c) => {
+		return c.every(v => v !== null);
+	}
 
-	console.log('closest coords', closestCoords);
 
 
 	return (
@@ -32,6 +48,23 @@ const GMap = () => {
 			onClick={handleCoordChange}
 			defaultOptions={{ styles: mapStyles }}
 		>
+			<Polyline path={PolylineCoordinates} options={{
+				strokeColor: '#0d6efd',
+				strokeOpacity: 0.5,
+				strokeWeight: 1,
+			}} />
+
+			{canDraw(lineBetween) && (
+				<Polyline
+					path={lineBetween}
+					options={{
+						strokeColor: 'green',
+						strokeOpacity: 0.5,
+						strokeWeight: 1,
+					}}
+				/>
+			)}
+
 			<Marker position={{ lat, lng: long }} />
 			{closestCoords.latClosest && (
 				<Marker
